d3bc0f2d6460bd53b791bb740aed3a2f48cbb742
Nobuyuki-Tokuchi <svalk.and.belnia.65535@gmail.com>
2017-03-18 15:16:41 +0900

    DivideHahを追加

--- WordCompress.hs
+++ WordCompress.hs
@@ -1,7 +1,10 @@
 module WordCompress(
   WordCompress(..), compress,
-  defaultOrder, defaultConsonant, defaultVowel
+  defaultOrder, defaultConsonants, defaultVowels
 ) where
+
+import qualified Data.List as List;
+import qualified Data.Ord as Ord;
 
 data WordCompress =
   Hah { width :: Int } |
@@ -10,12 +13,20 @@ data WordCompress =
 hah_compress :: [a] -> Int -> [a]
 hah_compress [] _  = []
 hah_compress [ch] _ = [ch]
-hah_compress str width = [head x, last x] ++ hah_compress (drop width str) width
-  where x = take width str
+hah_compress str width = let x = take width str
+                         in [head x, last x] ++ hah_compress (drop width str) width
+
+divhah_compress :: WordCompress -> String -> String
+divhah_compress (DivideHah width consonants vowels) str = map (\x@(ch, _) -> ch) $ List.sortBy (Ord.comparing snd) $ divcv $ zip str [0..]
+  where divcv tlist = let clist = filter (\x@(str, _) -> elem str consonants) tlist
+                          vlist = filter (\x@(str, _) -> elem str vowels) tlist
+                      in hah_compress clist width ++ hah_compress vlist width
 
 compress :: WordCompress -> String -> String
 compress (Hah width) str = hah_compress str width
+compress divhah str = divhah_compress divhah str
 
 defaultOrder = "abcdefghijklmnopqrstuvwxyz"
-defaultConsonant = "bcdfghjklmnpqrstvwxyz"
-defaultVowel = "aeiou"
+defaultConsonants = "bcdfghjklmnpqrstvwxyz"
+defaultVowels = "aeiou"
+
--- hah.hs
+++ hah.hs
@@ -6,34 +6,66 @@ data Option =
   | Help
   | Parameters {
       count :: Int,
-      word :: String
+      word :: String,
+      consonants :: String,
+      vowels :: String,
+      mode :: String
     }
 
 defaultOption = Parameters {
     count = 4,
-    word = ""
+    word = "",
+    consonants = WComp.defaultConsonants,
+    vowels = WComp.defaultVowels,
+    mode = "normal"
   }
 
 getOption :: [String] -> Option -> Either String Option
 getOption [] Parameters{ word = "" } = Right Help
 getOption [] option = Right option
 getOption ("-h":xs) _ = Right Help
-getOption ["-c"] _ = Left "argument to '-c' is missing "
+
+getOption ["-c"] _ = Left "argument to '-c' is missing"
 getOption ("-c":val:xs) option = getOption xs option{ count = (read val) }
+
+getOption ("--normal":xs) option = getOption xs option { mode = "normal" }
+getOption ("-n":xs) option = getOption xs option { mode = "normal" }
+getOption ("--divide":xs) option = getOption xs option { mode = "divide" }
+getOption ("-d":xs) option = getOption xs option { mode = "divide" }
+
+getOption ["--consonants"] _ = Left "argument to '--consonants' is missing"
+getOption ("--consonants":val:xs) option = getOption xs option { consonants = val }
+getOption ["-C"] _ = Left "argument to '-C' is missing"
+getOption ("-C":val@(h:_):xs) option = case h of
+  '-' -> Left "argument to '-C' is missing"
+  otherwise -> getOption xs option { consonants = val }
+getOption ["--vowels"] _ = Left "argument to '--vowels' is missing"
+getOption ("--vowels":val:xs) option = getOption xs option { vowels = val }
+getOption ["-V"] _ = Left "argument to '-V' is missing"
+getOption ("-V":val:xs) option = getOption xs option { vowels = val }
+
 getOption (inWord:xs) option = getOption xs option{ word = inWord }
 
 help :: IO()
 help = do
-  putStrLn "hah (-c n) word"
-  putStrLn "-c n : Split width. (default) -c 4"
+  putStrLn $ concatMap (\x -> x ++ "\n") [
+      "hah (-n|-d) (-C) (-V) (-c n) word",
+      "  -c n : Split width. (default) -c 4",
+      "  -n, --normal : Hah Compress. (default)",
+      "  -d, --divide : DivideHah Compress.",
+      "  -C, --consonants : Set consonants. (DivideHah only) (default) -C bcdfghjklmnprqstvwxyz",
+      "  -V, --vowels : Set consonants. (DivideHah only) (default) -V aeiou"
+    ]
 
 run :: Either String Option -> IO()
 run (Left message) = putStrLn message
 run (Right Help) = help
-run (Right Parameters{ count = c, word = w }) = putStrLn $ WComp.compress (WComp.Hah c) w
+run (Right Parameters { count = c, word = w, mode = m, consonants = cs, vowels = vs }) = putStrLn $ WComp.compress compMode w where
+  compMode = case m of
+    "divide" -> (WComp.DivideHah c cs vs)
+    otherwise -> (WComp.Hah c)
 
 main = do
   args <- getArgs
   run $ getOption args defaultOption
-
 
