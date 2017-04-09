b3cbda694eef5de9adac9a9019ce057447829caf
Nobuyuki-Tokuchi <svalk.and.belnia.65535@gmail.com>
2017-04-09 17:45:30 +0900

    余分な変数定義を削除

--- WordCompress.hs
+++ WordCompress.hs
@@ -1,6 +1,7 @@
 module WordCompress(
   WordCompress(..), compress,
-  defaultOrder, defaultConsonants, defaultVowels
+  --defaultOrder,
+  defaultConsonants, defaultVowels
 ) where
 
 import qualified Data.List as List;
@@ -17,16 +18,17 @@ hah_compress str width = let x = take width str
                          in [head x, last x] ++ hah_compress (drop width str) width
 
 divhah_compress :: WordCompress -> String -> String
-divhah_compress (DivideHah width consonants vowels) str = map (\x@(ch, _) -> ch) $ List.sortBy (Ord.comparing snd) $ divcv $ zip str [0..]
-  where divcv tlist = let clist = filter (\x@(str, _) -> elem str consonants) tlist
-                          vlist = filter (\x@(str, _) -> elem str vowels) tlist
+divhah_compress (DivideHah width consonants vowels) str =
+  map (\(ch, _) -> ch) $ List.sortBy (Ord.comparing snd) $ divcv $ zip str [0..]
+  where divcv tlist = let clist = filter (\(str, _) -> elem str consonants) tlist
+                          vlist = filter (\(str, _) -> elem str vowels) tlist
                       in hah_compress clist width ++ hah_compress vlist width
 
 compress :: WordCompress -> String -> String
 compress (Hah width) str = hah_compress str width
 compress divhah str = divhah_compress divhah str
 
-defaultOrder = "abcdefghijklmnopqrstuvwxyz"
+--defaultOrder = "abcdefghijklmnopqrstuvwxyz"
 defaultConsonants = "bcdfghjklmnpqrstvwxyz"
 defaultVowels = "aeiou"
 
