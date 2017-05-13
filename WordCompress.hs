d0e1e8daf5871bf3f08be319bd256158d1f058ef
Nobuyuki-Tokuchi <svalk.and.belnia.65535@gmail.com>
2017-05-14 03:43:21 +0900

    行列圧縮系を追加

--- WordCompress.hs
+++ WordCompress.hs
@@ -9,24 +9,45 @@ import qualified Data.Ord as Ord;
 
 data WordCompress =
   Hah { width :: Int } |
-  DivideHah { width :: Int, consonants :: String, vowels :: String }
+  DivideHah { width :: Int, consonants :: String, vowels :: String } |
+  Matrix |
+  DivMatrix { consonants :: String, vowels :: String }
 
-hah_compress :: [a] -> Int -> [a]
-hah_compress [] _  = []
-hah_compress [ch] _ = [ch]
-hah_compress str width = let x = take width str
-                         in [head x, last x] ++ hah_compress (drop width str) width
+hah_compress :: Int -> [a] -> [a]
+hah_compress _ []  = []
+hah_compress _ [ch] = [ch]
+hah_compress width str = let x = take width str
+                         in [head x, last x] ++ hah_compress width (drop width str)
+
+matrix_compress :: [a] -> [a]
+matrix_compress [] = []
+matrix_compress str = [head str] ++ next_compress (fromIntegral $ ceiling $ sqrt $ fromIntegral $ length str) (tail str)
+  where next_compress _ [] = []
+        next_compress size str
+                      | size >= length str = [last str]
+                      | otherwise = let nextStr = drop size str
+                                    in [head nextStr] ++ next_compress size (tail nextStr)
+
+divcv consonants vowels tlist =
+  let clist = filter (divider consonants) tlist
+      vlist = filter (divider vowels) tlist
+  in (clist, vlist) where divider param = \(str, _) -> elem str param
 
 divhah_compress :: WordCompress -> String -> String
 divhah_compress (DivideHah width consonants vowels) str =
-  map (\(ch, _) -> ch) $ List.sortBy (Ord.comparing snd) $ divcv $ zip str [0..]
-  where divcv tlist = let clist = filter (\(str, _) -> elem str consonants) tlist
-                          vlist = filter (\(str, _) -> elem str vowels) tlist
-                      in hah_compress clist width ++ hah_compress vlist width
+  map (\(ch, _) -> ch) $ List.sortBy (Ord.comparing snd) $ compress $ divcv consonants vowels $ zip str [0..]
+  where compress (clist, vlist) = hah_compress width clist ++ hah_compress width vlist
+
+divmatrix_compress :: WordCompress -> String -> String
+divmatrix_compress (DivMatrix consonants vowels) str = 
+  map (\(ch, _) -> ch) $ List.sortBy (Ord.comparing snd) $ compress $ divcv consonants vowels $ zip str [0..]
+  where compress (clist, vlist) = matrix_compress clist ++ matrix_compress vlist
 
 compress :: WordCompress -> String -> String
-compress (Hah width) str = hah_compress str width
-compress divhah str = divhah_compress divhah str
+compress (Hah width) str = hah_compress width str
+compress (Matrix) str = matrix_compress str
+compress divhah@(DivideHah _ _ _) str = divhah_compress divhah str
+compress divmatrix@(DivMatrix _ _) str = divmatrix_compress divmatrix str
 
 --defaultOrder = "abcdefghijklmnopqrstuvwxyz"
 defaultConsonants = "bcdfghjklmnpqrstvwxyz"
--- hah.hs
+++ hah.hs
@@ -34,6 +34,10 @@ getOption ("--normal":xs) option = getOption xs option { mode = "normal" }
 getOption ("-n":xs) option = getOption xs option { mode = "normal" }
 getOption ("--divide":xs) option = getOption xs option { mode = "divide" }
 getOption ("-d":xs) option = getOption xs option { mode = "divide" }
+getOption ("--matix":xs) option = getOption xs option { mode = "matrix" }
+getOption ("-m":xs) option = getOption xs option { mode = "matrix" }
+getOption ("--divmat":xs) option = getOption xs option { mode = "divmat" }
+getOption ("-dm":xs) option = getOption xs option { mode = "divmat" }
 -- consonants option
 getOption [op@"--consonants"] _ = Left ("argument to '" ++ op ++ "' is missing")
 getOption (op@"--consonants":val@(h:_):xs) option = case h of
@@ -62,6 +66,8 @@ help = do
       "  -c n : Split width. (default) -c 4",
       "  -n, --normal : Hah Compress. (default)",
       "  -d, --divide : DivideHah Compress.",
+      "  -m, --matrix : Matix Compress.",
+      "  -dm, --divmat : DivideMatix Compress.",
       "  -C, --consonants : Set consonants. (DivideHah only) (default) -C bcdfghjklmnprqstvwxyz",
       "  -V, --vowels : Set consonants. (DivideHah only) (default) -V aeiou"
     ]
@@ -72,6 +78,8 @@ run (Right Help) = help
 run (Right Parameters { count = c, word = w, mode = m, consonants = cs, vowels = vs }) = putStrLn $ WComp.compress compMode w where
   compMode = case m of
     "divide" -> (WComp.DivideHah c cs vs)
+    "divmat" -> (WComp.DivMatrix cs vs)
+    "matrix" -> (WComp.Matrix)
     otherwise -> (WComp.Hah c)
 
 main = do
